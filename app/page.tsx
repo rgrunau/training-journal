2a09acb221764014b4e47fdfb0f1b3560cb5f118
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-11-15 10:30:36 -0800

    setting up clark routes

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,24 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center lg:items-start py-24 px-36 bg-slate-900 text-slate-50">
-      <div className="w-full lg:w-1/3">
-        <h1 className="text-7xl">Hippy Training Journal</h1>
-      </div>
-      <div className="text-5xl lg:w-2/3">
-        <p>
-          An ai backed Training Journal for people who like to talk about their
-          feelings.
-        </p>
-      </div>
-      <div className="mt-4 py-3">
-        <Link
-          href="/login"
-          className="bg-slate-700 hover:bg-slate-800 text-slate-50 text-3xl px-4 py-2 rounded-md border-2 border-slate-50"
-        >
-          Get Started
-        </Link>
+    <main className="flex w-full h-screen items-center justify-center py-24 px-36 bg-slate-900 text-slate-50">
+      <div className="w-full lg:w-2/3">
+        <div className="my-4">
+          <h1 className="text-6xl font-regular">Personal Dashboard</h1>
+        </div>
+        <div className="my-2">
+          <h2 className="text-2xl font-thin text-slate-100">
+            A dashboard to track the things I want to track
+          </h2>
+        </div>
+        <div className="mt-4 py-3">
+          <Link
+            href="sign-up"
+            className="bg-slate-100 text-slate-900 text-3xl px-4 py-2 rounded-md font-regular"
+          >
+            Get Started
+          </Link>
+        </div>
       </div>
     </main>
   )
